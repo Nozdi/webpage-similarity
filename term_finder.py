30b1dd75df11aed625611d0ab70ffaf49d8d3f28
Kacper Kaśków <kacper.kaskow@apilia.pl>
2014-04-14 21:36:57 +0200

    added functions for extracting terms for given files/file

--- term_finder.py
+++ term_finder.py
@@ -4,7 +4,7 @@
 import sys
 from topia.termextract import extract
 
-def exctract(text):
+def extract_terms_from_text(text):
     """
             Finds terms in text and returns them as list of tuples
             ( term, term amount, term length in  words)
@@ -13,10 +13,33 @@ def exctract(text):
     extractor.filter = extract.permissiveFilter
     return extractor(text)
 
+def extract_all_terms_from_files(filenames):
+    """
+        Finds terms in text files and returns them as list
+    """
+    words = set()
+    for filename in filenames:
+        print filename,"\n"
+        words |= set(extract_terms_from_file(filename))
+    return list(words)
+
+def extract_terms_from_file(filename):
+    """
+        Finds terms in text file and returns them as list
+    """
+    with open (filename, "r") as myfile:
+        text=myfile.read().replace('\n', ' ')
+        extracted = [touple[0] for touple in extract_terms_from_text(text)]
+        return extracted
+
+
 if __name__ == "__main__":
     argv=sys.argv
-    with open (argv[1], "r") as myfile:
-        text=myfile.read().replace('\n', ' ')
-    extracted =  exctract(text)
-    print len(extracted)
-    print extracted
+    result =  extract_all_terms_from_files([argv[1]])
+    print len(result)
+    print result
+    #with open (argv[1], "r") as myfile:
+    #    text=myfile.read().replace('\n', ' ')
+    #extracted =  exctract(text)
+    #print len(extracted)
+    #print extracted
