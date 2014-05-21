c2177accc5fefffea43b56358d4eedd50bf6261d
Mateusz Flieger <mateuszflieger@gmail.com>
2014-05-21 11:50:54 +0200

    Removed unicode from texts

--- tools.py
+++ tools.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     files = glob("./texts/*/*.txt")
     for filename in files:
         with codecs.open(filename, "r+", encoding="utf-8") as f:
-            new_text = unicode(f.read())
+            new_text = unidecode(f.read())
             f.seek(0)
             f.truncate()
             f.write(new_text)
